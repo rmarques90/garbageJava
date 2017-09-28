88cde580fa68aed6a859d43ae23e182b4576bc59
R Marques <rafamdd@gmail.com>
2017-09-27 21:42:54 -0300

    Update com função objetiva

---
+++ src/ObjetiveFunction.java
@@ -0,0 +1,14 @@
+/**
+ * Created by fox on 27/09/17.
+ */
+public class ObjetiveFunction {
+    private double finalDistance;
+    private double finalTime;
+    private double finalVelocity;
+
+    public ObjetiveFunction (double distance, double time, double mediumVelocity) {
+        finalDistance = distance;
+        finalTime = time;
+        finalVelocity = mediumVelocity;
+    }
+}
