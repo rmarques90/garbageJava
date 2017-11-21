5be7a199c63f79e7878f4e9591e41963eb51dc25
rmarques90 <rafamdd@gmail.com>
2017-11-21 16:21:14 -0200

    API Google Maps working

--- src/main/java/MapsAPI/Maps.java
+++ src/main/java/MapsAPI/Maps.java
@@ -2,6 +2,7 @@ package MapsAPI;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -12,13 +13,14 @@ import java.net.URL;
  */
 public class Maps {
 
-    private static String key =  "AIzaSyAQOmyCzcNMFx8h5c3bGifWfXMEW4oqyTo";
+    private static String keyGeo =  "AIzaSyAQOmyCzcNMFx8h5c3bGifWfXMEW4oqyTo";
+    private static String keyPlace =  "AIzaSyDOg-dJ-iwOf1jDyiwsR4TFor0bD0BYLoU";
 
-    private static void saveCoordinates() throws MalformedURLException {
+    private static void getGeocoding() throws MalformedURLException {
 //        https://developers.google.com/maps/documentation/geocoding/intro#GeocodingRequests
         try {
-            String address = "Goiânia&types=neighborhood";
-            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + key);
+            String address = "Goiânia";
+            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + keyGeo);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
@@ -45,8 +47,44 @@ public class Maps {
         }
     }
 
+    private static void getNearbySearch() {
+//        https://developers.google.com/places/web-service/intro
+        String params = "";
+        try {
+            URL url = new URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + params + "&key=" + keyPlace);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            try {
+                File file = new File(System.getProperty("java.io.tmpdir") + File.separator + "teste.json");
+                InputStream in = connection.getInputStream();
+                FileOutputStream fos = new FileOutputStream(file);
+
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = in.read(buffer)) != -1) {
+                    fos.write(buffer, 0, length);
+                }
+                in.close();
+                fos.flush();
+                fos.close();
+                System.out.print("Arquivo Salvo");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws MalformedURLException {
-        saveCoordinates();
+        getNearbySearch();
+        //        https://www.quora.com/How-do-you-get-boundaries-of-neighborhoods-via-the-Google-Maps-api
+
+
     }
 
 
